c0cc87ea24a456e7e95cdb14c4ae84120c0fbcc5
AAA <aaa@aaa.com>
2017-04-07 21:06:42 -0500

    Combined bot2.js into bot.js, got errors other way

--- bot.js
+++ bot.js
@@ -60,3 +60,72 @@ else{
 
 
 //module.exports = Bot;
+
+
+//---------------------------------------------------------------------
+//retweeting bot
+var TWITTER_SEARCH_PHRASE = '#yomama OR #YoMama';
+
+
+var Bot2 = new Twit({
+	consumer_key: process.env.BOT_CONSUMER_KEY,
+ consumer_secret: process.env.BOT_CONSUMER_SECRET,
+ access_token: process.env.BOT_ACCESS_TOKEN,
+ access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
+});
+
+console.log('bot2 is running...');
+
+/* BotInit() : To initiate the bot */
+function BotInit() {
+	Bot2.post('statuses/retweet/:id', { id: '669520341815836672' }, BotInitiated);
+	
+	function BotInitiated (error, data, response) {
+		if (error) {
+			console.log('Bot2 could not be initiated, : ' + error);
+		}
+		else {
+  			console.log('Bot2 initiated : 669520341815836672');
+		}
+	}
+	
+	BotRetweet();
+}
+
+/* BotRetweet() : To retweet the matching recent tweet */
+function BotRetweet() {
+
+	var query = {
+		q: TWITTER_SEARCH_PHRASE,
+		result_type: "recent"
+	}
+
+	Bot2.get('search/tweets', query, BotGotLatestTweet);
+
+	function BotGotLatestTweet (error, data, response) {
+		if (error) {
+			console.log('Bot could not find latest tweet, : ' + error);
+		}
+		else {
+			var id = {
+				id : data.statuses[0].id_str
+			}
+
+			Bot2.post('statuses/retweet/:id', id, BotRetweeted);
+			
+			function BotRetweeted(error, response) {
+				if (error) {
+					console.log('Bot could not retweet, : ' + error);
+				}
+				else {
+					console.log('Bot retweeted : ' + id.id);
+				}
+			}
+		}
+	}
+	//every 24 hours
+	setInterval(BotRetweet, 24*60*60*1000);
+}
+
+/* Initiate the Bot */
+BotInit();
